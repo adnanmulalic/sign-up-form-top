427b5e09de798d776fd9b45e727c5841b1788f27
adnanmulalic <adnan.mulalic3@gmail.com>
2022-09-30 10:41:01 +0200

    done

--- javascript.js
+++ javascript.js
@@ -1,11 +1,30 @@
 const password = document.querySelector("[name='password']");
 const confirmPassword = document.querySelector("[name='confirm-password']");
+const hiddenParagraph = document.querySelector("#hidden-paragraph");
 let passOne = "";
 let passTwo = "";
 
-password.addEventListener("change", () => {
+password.addEventListener("keyup", () => {
     passOne = password.value;
+    if (passOne && passTwo != "" && passTwo === passOne) {
+        password.style.outline = "1px solid green";
+        confirmPassword.style.outline = "1px solid green";
+        hiddenParagraph.setAttribute("hidden", "");
+    } else {
+        password.style.outline = "1px solid red";
+        confirmPassword.style.outline = "1px solid red";
+        hiddenParagraph.removeAttribute("hidden");
+    }
 });
-confirmPassword.addEventListener("change", () => {
+confirmPassword.addEventListener("keyup", () => {
     passTwo = confirmPassword.value;
-})
+    if (passTwo && passOne != "" && passTwo === passOne ) {
+        password.style.outline = "1px solid green";
+        confirmPassword.style.outline = "1px solid green";
+        hiddenParagraph.setAttribute("hidden", "");
+    } else {
+        password.style.outline = "1px solid red";
+        confirmPassword.style.outline = "1px solid red";
+        hiddenParagraph.removeAttribute("hidden");
+    }
+});
